20055a3ff624e19ae9450838e4c237bf047a55bd
zhangfan <brian@qvod.com>
2014-05-26 20:54:15 +0800

    fix settings

--- OperatorMan/configs/settings.py
+++ OperatorMan/configs/settings.py
@@ -2,13 +2,13 @@
 
 DEBUG=True
 
-OPERATOR_SERVER_IP = '172.16.10.183'
+OPERATOR_SERVER_IP = '127.0.0.1'
 OPERATOR_SERVER_PORT = 8801
 
 SECRET_KEY = '96e2b3699a852ade9d4d2fd408c93612'
 
-COOKIES_DOMAIN = '172.16.10.183' # cookies 域
-HOME_DOMAIN = '172.16.10.183' # 主域
+COOKIES_DOMAIN = '127.0.0.1' # cookies 域
+HOME_DOMAIN = '127.0.0.1' # 主域
 
 P_LOGIN_URI = '/permission/login/'
 P_QUERY_SESSION_URI = '/permission/session/query/'
--- README.md
+++ README.md
@@ -20,3 +20,9 @@ $ easy_install Flask-Login
 MySql-Python
 
 $ easy_install mysql-python
+
+Windows PYTHON_PATH SET
+
+http://yuji.wordpress.com/2009/08/31/python-on-windows-setting-pythonpath-environment-variable/
+
+$
