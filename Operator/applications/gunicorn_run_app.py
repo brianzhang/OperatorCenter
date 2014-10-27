5068a2419b2ad03204a2a706850c38452c03a495
brian <brian@qvod.com>
2014-10-27 11:22:38 +0800

    fix

---
+++ Operator/applications/gunicorn_run_app.py
@@ -0,0 +1,18 @@
+#coding=utf-8
+import logging
+from Operator.configs import settings
+
+
+
+bind =  "%s:%s" % (settings.OPERATOR_SERVER_IP, settings.OPERATOR_SERVER_PORT)
+workers = 2
+worker_connections = 100
+#worker_class = "gevent"
+worker_class = "sync"
+backlog = 2048
+debug = settings.SYS_DEBUG
+log_level = logging.ERROR
+daemon = True
+pidfile = "/root/logs/operator_man.pid"
+logfile =  "/root/logs/operator_man.log"
+#max_requests = 10000
